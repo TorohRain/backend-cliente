2f522ab3758dfbd85ab54466d420ef85e5b8ef23
Rain <dudu.silva7@gmail.com>
2021-03-24 15:26:15 -0300

    backend-cliente

--- src/routes/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const ClienteController = require ('C:\\Users\\User\\backend-cliente\\src\\routes\\routes.js');
-const router = express.Router();
- 
-router.post('C:\\Users\\User\\backend-cliente\\index.js', ClienteController.Insert);
-router.get('/cliente', ClienteController.SelectAll);
-router.get('/cliente/:id', ClienteController.SelectDetail);
-
-module.exports = router;
