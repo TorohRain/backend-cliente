dfef78ad2440768d365e39eed612b87e2d84ce9e
TorohRain <dudu.silva7@gmail.com>
2021-05-05 15:11:45 -0300

    Backend com Update e Delete

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const ClienteController = require ('C:\\Users\\User\\backend-cliente\\src\\routes\\routes.js');
+const ClienteController = require ('../controllers/ClienteController.js');
 const router = express.Router();
  
 router.post('C:\\Users\\User\\backend-cliente\\index.js', ClienteController.Insert);
